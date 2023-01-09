35622af7712d9a59ab0bd3467710ad02a8b6cf3b
EugeneStone <89702595+EugeneStone@users.noreply.github.com>
2023-01-09 22:43:16 +0300

    added dynamic of buttons; added event listeners on buttons

---
+++ public/main.js
@@ -0,0 +1,10 @@
+const buttonCreateAccount = document.getElementById('button-create-account');
+const buttonSignIn = document.getElementById('button-sign-in');
+
+buttonCreateAccount.addEventListener('click', () => {
+    alert('working create');
+})
+
+buttonSignIn.addEventListener('click', () => {
+    alert('working signIn');
+})
